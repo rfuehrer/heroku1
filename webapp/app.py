71adbd8c6b145b3c6ffc88351759643bf1e013a3
Rene Fuehrer <rene.fuehrer@generali.com>
2020-08-20 12:35:52 +0200

    *

---
+++ webapp/app.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__, static_url_path='/webapp')
+
+@app.route('/')
+#def hello():
+#    return 'Hello world!'
+def root():
+    return app.send_static_file('index.html')
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
