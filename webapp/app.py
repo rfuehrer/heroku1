2f31911fd8abd5d6363e945a67675f410bff0463
Rene Fuehrer <rene.fuehrer@generali.com>
2020-08-20 12:53:37 +0200

    *

--- webapp/app.py
+++ webapp/app.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask
 
-app = Flask(__name__, static_url_path='/opt/webapp')
+app = Flask(__name__, static_folder='./', static_url_path='/'))
 
 @app.route('/')
 #def hello():
