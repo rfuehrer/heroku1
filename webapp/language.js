a21d4abf3fd08ae0408f20c4e2c32b5191472f76
Rene Fuehrer <rene.fuehrer@generali.com>
2020-08-20 21:04:31 +0200

    *

---
+++ webapp/language.js
@@ -0,0 +1,18 @@
+var language = {
+  intro:{
+    title: "Hintergrund",
+    help: "Auf dieser Seite wird der Verlauf des Team-Rankings dargestellt. Möchtest Du Dich an dem Team beteiligen? Sehr gerne, eine Anleitung zur Einrichtung des Folding@Home-Clients findest Du <a href='https://rfuehrer.gitbook.io/red-lions/' target=_blank>hier auf unserer Gitbook.io-Seite</a>. Wir freuen uns über jeden Mitwirkenden, egal ob Kolleg:inn, Familienmitglied, Partner:in oder Freund:in. Aber auch alle anderen dürfen natürlich herzlich gerne mitmachen!"
+  },
+  progress:{
+      title: "Verlauf",
+      help: "Der Verlauf stellt die grafische Auswertung aller Veränderung im Team-Ranking dar. Die Anzeige umfasst die Entwicklung der vergangenen Tage . Über die Schaltfläche kann die Grafik als Bilddatei gesichert werden."
+    },
+    history:{
+      title: "Historische Werte",
+      help: "Die historischen Werte stellen alle Änderungen des Rankings in einer tabellarischen Übersicht dar. Die Prüfung erfolgt in der Regel jede Minute. Es werden jedoch nur Einträge in dieser Übersicht vorgenommen, wenn eine Änderung im Ranking festgestellt wurde."
+    },
+    supporter:{
+      title: "Unterstützer",
+      help: "Die Übersicht stellt die für eine Änderung im Ranking beisteuernden IDs dar. Es werden hier aktive Team-Mitglieder mit einem eigenen Rang aufgeführt. Die genannten Mitwirkdenden haben während der letzten Veränderung des Team-Rankings sogenannte 'Credits' für das Team errechnet und damit zur Veränderung der Platzierung beigetragen. Bei negativen Veränderungen des Team-Rankings haben diese Mitglieder aktiv dabei unterstützt, dass die Veränderung im Sinne des Teams geringer ausfiel."
+    }
+  }
