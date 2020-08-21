02c3719119f8b719fc1d421ce1f7174651291abe
Rene Fuehrer <rene.fuehrer@generali.com>
2020-08-21 08:03:27 +0200

    *

--- webapp/clock.py
+++ webapp/clock.py
@@ -7,7 +7,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every one minutes.')
-    os.spawnl(os.P_DETACH, '/opt/webapp/folding-stats.py')
+    os.spawnl(os.P_NOWAIT, '/opt/webapp/folding-stats.py')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
