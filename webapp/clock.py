4e7423d2020d418a9619810a819223fa0e3e5142
Rene Fuehrer <rene.fuehrer@generali.com>
2020-08-21 08:17:34 +0200

    *

--- webapp/clock.py
+++ webapp/clock.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python3
-import os
+#import os
+import subprocess
 from apscheduler.schedulers.blocking import BlockingScheduler
+
+
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every one minutes.')
-    os.spawnl(os.P_NOWAIT, '/opt/webapp/folding-stats.py')
+#    os.spawnl(os.P_NOWAIT, '/opt/webapp/folding-stats.py')
+    pid = subprocess.Popen(["python3", "/opt/webapp/folding-stats.py"], creationflags=subprocess.DETACHED_PROCESS)
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
